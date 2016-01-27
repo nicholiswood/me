4046567e9e1de44f25caa5c1d8b973510da941d0
Nicholis Wood <nicholis430@gmail.com>
2016-01-26 21:11:44 -0800

    New index
    Trying to add updated site

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+<title>Nwood || About me</title>
+<meta content='width=500' name='viewport'>
+<link href="../assets/fonts/font-awesome.min.css" media="screen" rel="stylesheet" type="text/css"/>
+<link href="../assets/css/bootstrap.min.css" media="screen" rel="stylesheet" type="text/css"/>
+<link href="../assets/css/application.css" media="screen" rel="stylesheet" type="text/css"/>
+<script src="../assets/js/jquery-1.10.2.js" type="text/javascript"></script>
+<script src="../assets/js/bootstrap.js" type="text/javascript"></script>
+<script src="../assets/js/application.js" type="text/javascript"></script>
+<link rel="icon" href="../assets/img/favicon.ico" type="image/x-icon" />
+</head>
+<body>
+	<div class='wrapper'>
+		<div class='wrapper-inner'>
+			<div class='circle'>
+				<a class='center' data-toggle='modal' href='#about'>
+					<img rel='tt-top' src='../assets/img/nicholis-circle.png' title='About Nick.'>
+				</a>
+				<a class='r' data-rot='0' href='http://instagram.com/nicholis_wood' target='_blank'>
+					<i class='fa fa-instagram' rel='tt-left' title='Instagram'></i>
+				</a>
+				<a class='r' data-rot='45' href='http://reddit.com/u/lessin' target='_blank'>
+					<i class='fa fa-reddit-square' rel='tt-left' title='Reddit'></i>
+				</a>
+				<a class='r' data-rot='90' href='https://facebook.com/nicholis.wood' target='_blank'>
+					<i class='fa fa-facebook' rel='tt-top' title='Facebook'></i>
+				</a>
+				<a class='r' data-rot='135' href='https://twitter.com/nicholiswood' target='_blank'>
+					<i class='fa fa-twitter' rel='tt-right' title='@nicholiswood'></i>
+				</a>
+				<a class='r' data-rot='180' href='https://youtube.com/user/towlsvids' target='_blank'>
+					<i class='fa fa-youtube-play' rel='tt-right' title='YouTube'></i>
+				</a>
+				<a class='r' data-rot='225' href='http://199.116.238.64' target='_blank'>
+					<i class='fa fa-home' rel='tt-right' title='Home'></i>
+				</a>
+				<a class='r' data-rot='270' href='http://twitch.tv/towls' target='_blank'>
+					<i class='fa fa-twitch' rel='tt-bottom' title='TwitchTV'></i>
+				</a>
+				<a class='r' data-rot='315' href='/contact.php' target='_blank'>
+					<i class='fa fa-send' rel='tt-left' title='Contact Form'></i>
+				</a>
+			</div>
+		</div>
+	</div>
+	<div class='modal fade' id='about'>
+		<div class='modal-dialog'>
+			<div class='modal-content'>
+				<div class='modal-header'>
+				<button class='close' data-dismiss='modal'>&times;</button>
+					<h4 class='modal-title'>About Nick</h4>
+				</div>
+				<div class='modal-body'>
+					<p>Hey, my name is Nicholis Wood</p>
+					<p>
+					I'm an 16 year old that lives on
+						<a href='http://199.116.238.64/map/' target='_blank'>Catalina Island</a>,
+					off the coast of Long Beach, CA.
+					</p>
+					<p>
+					I'm also interested in playing a few different games in my free time.
+					</p>
+					<p>
+					I'm currently a Junior at
+						<a href='http://lbavalon.schoolloop.com' target='_blank'>Avalon High</a>
+					with plans to study Computer Science.
+					</p>
+					<p>You can find me around these places,</p>
+					<p id='icons'>
+						<a href='http://instagram.com/nicholis_wood' target='_blank'>
+					<i class='fa fa-2x fa-instagram' rel='tt-top' title='Instagram'></i>
+						</a>
+						<a href='http://reddit.com/u/lessin' target='_blank'>
+					<i class='fa fa-2x fa-reddit-square' rel='tt-top' title='Reddit'></i>
+						</a>
+						<a href='https://facebook.com/nicholis.wood' target='_blank'>
+					<i class='fa fa-2x fa-facebook' rel='tt-top' title='Facebook'></i>
+						</a>
+						<a href='https://twitter.com/nicholiswood' target='_blank'>
+					<i class='fa fa-2x fa-twitter' rel='tt-top' title='Twitter'></i>
+						</a>
+						<a href='https://youtube.com/user/towlsvids' target='_blank'>
+					<i class='fa fa-2x fa-youtube-play' rel='tt-top' title='YouTube'></i>
+						</a>
+						<a href='http://199.116.238.64' target='_blank'>
+					<i class='fa fa-2x fa-home' rel='tt-top' title='home'></i>
+						</a>
+						<a href='http://twitch.tv/towls' target='_blank'>
+					<i class='fa fa-2x fa-twitch' rel='tt-top' title='TwitchTV'></i>
+						</a>
+						<a href='/contact.php' target='_blank'>
+					<i class='fa fa-2x fa-send' rel='tt-top' title='Contact Form'></i>
+						</a>
+					</p>
+				</div>
+			</div>
+		</div>
+	</div>
+<script type="text/javascript">
+</script>
+</body>
+</html>
